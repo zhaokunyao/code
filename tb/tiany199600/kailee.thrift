b269da55c6554657b33d3b28bd1cad8a7bc5cf47
zhaokunyao <zhaokunyao@baidu.com>
2017-10-13 19:05:28 +0800

    add comment

--- tb/tiany199600/kailee.thrift
+++ tb/tiany199600/kailee.thrift
@@ -21,6 +21,8 @@
   * Ahh, now onto the cool part, defining a service. Services just need a name
   * and can optionally inherit from another service using the extends keyword.
   */
+  /**服务名称 Sorter 这个我理解可以随便写
+  **/
  service Sorter {
  
    /**
@@ -29,10 +31,32 @@
     * lists and exception lists are specified using the exact same syntax as
     * field lists in struct or exception definitions.
     */
+
+    /**
+     * 下面约定接口格式
+     * 所谓接口，就是服务端(server)提供哪些服务，以函数的形式来提供
+     * 客户端(client)可以调用这些接口，也就是调用这些函数。
+     * 函数的参数由客户端传入， 
+     * 函数的返回值由服务端返回给客户端。
+     **/
  
+    /**
+     * 第一个接口  ping  无参数， 无返回值所以是void
+     * 这个接口是我搞来玩的
+    **/
     void ping(),
  
+    /**
+     * 第二个接口，  add   参数是二个i32,就是2个整数。参数名分别是num1和num2
+     * 返回值也是i32， 也是个整数。
+     * 这个接口也是我搞来玩的
+     */
     i32 add(1:i32 num1, 2:i32 num2),
+
+    /**
+     *第三个接口， 参数是一个 list<double> 就是由double构成的数组。 参数名是 random_double_list
+     *它的返回值也是一个list<double> 就是由double构成的数组。
+     */
     list<double> sort_list(1:list<double> random_double_list)
  
  }
